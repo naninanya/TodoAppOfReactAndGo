515b1fefa8e1c508f494e4e91f4b58d08c658dc4
naninanya <qqzh3us9k@sweet.ocn.ne.jp>
2021-09-25 08:28:24 +0900

    remove TodoItem.tsx

--- client-app/src/components/TodoItems.tsx
+++ client-app/src/components/TodoItems.tsx
@@ -1,10 +1,24 @@
 import { AllItemsPropsType } from "../types";
-import TodoItem from "./TodoItem";
 
 const TodoItems = ({ items, setItems }: AllItemsPropsType) => {
+
+    const setCompleted = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const target = Number(e.currentTarget.id)
+        setItems(items.map((item) => {
+            if (item.Id === target)
+                item.isCompleted = !item.isCompleted;
+            return item;
+        }))
+    }
+
     return (
         <>
-            {items.map((item) => <TodoItem item={item} items={items} setItems={setItems} />)}
+            {items.map((item) =>
+                <button className={!item.isCompleted ? "ItemButton" : "CompletedItemButton"}
+                    type="submit" onClick={setCompleted} id={item.Id.toString()}>
+                    {!item.isCompleted ? item.Name : item.Name + " Complete!"}
+                </button>
+            )}
         </>
     );
 };
