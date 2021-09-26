bc5ab47d11938bf4fef714ba0505d761c5b0f28a
naninanya <qqzh3us9k@sweet.ocn.ne.jp>
2021-09-27 00:08:26 +0900

    echo on docker

--- echo/server.go
+++ echo/server.go
@@ -24,8 +24,8 @@ func main() {
 	e.DELETE("/api/delete", handler.Delete())
 
 	// Start server
-	// If you want to check from iphone, you change to :1323.
-	e.Logger.Fatal(e.Start("localhost:1323"))
+	// If you want to check from iphone or on Docker, you change to :1323.
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 // Handler
---
+++ postgres/migration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+psql -U postgres << EOSQL
+CREATE TABLE TodoItem (
+    id SERIAL NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (id)
+);
+EOSQL
